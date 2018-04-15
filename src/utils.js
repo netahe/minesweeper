0b97b460fde42337f1834bf7c21410ee549e2406
Neta Hemyan <neta.he@gmail.com>
2018-04-15 18:01:39 +0300

    added game utils file

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+import {BoardModel} from "./model/board";
+
+const GAMEOVER = Symbol();
+
+const NUMBER = Symbol();
+const MINE = Symbol();
+const EMPTY = Symbol();
+
+
+
+// return a random integer in  [0, range)
+export function randint(range) {
+    return Math.floor(Math.random() * range);
+}
