1e36d3255fe684ff366cc8ecf37e7e3530cbf4f9
Neta Hemyan <neta.he@gmail.com>
2018-04-17 19:01:03 +0300

    added game exceptions

---
+++ src/model/errors.js
@@ -0,0 +1,20 @@
+export class GameError extends Error {
+
+}
+
+export class SteppedOnMine extends GameError {
+
+}
+
+// winning the game is not technically an error, but it is something that stops the normal flow of the game,
+// specifically, by ending it.
+export class GameWon extends GameError {
+
+}
+
+export class NotEnoughFlags extends GameError {
+
+}
+
+export class CellAlreadyFlagged extends GameError {}
+
