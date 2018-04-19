aeb4bcf37337c80588e94974628ed62727330ebe
Neta Hemyan <neta.he@gmail.com>
2018-04-19 22:10:59 +0300

    added TODO, I don't think I have time to get into this one.

--- src/utils/array2d.js
+++ src/utils/array2d.js
@@ -1,18 +1,32 @@
+/**
+ * TODO: I want to user this class to abstract away some of the low-level board operation, but I'm not sure I have time to get into that.
+ */
+
 class Array2D {
     constructor(rows, cols) {
         this.rows = rows;
         this.cols = cols;
-        this.data = [];
+        this.data = Array(rows);
+
+        for(let i=0; i < this.rows; i++) {
+            this.data[i] = Array(cols);
+
+            for(let j=0;  j < this.cols; j++){
+                this.data[i][j] = {}
+            }
+        }
     }
 
     forEachRow(func) {
-
+        for(let i=0; i < this.rows; i++) {
+            func(i, this.data[i]);
+        }
     }
 
     forEachElement(func) {
         for(let i=0 ; i < this.rows; i++) {
             for(let j=0; j < this.cols; j++) {
-                func(i,j, thid.data[i][j]);
+                func(i,j, this.data[i][j]);
             }
         }
     }
