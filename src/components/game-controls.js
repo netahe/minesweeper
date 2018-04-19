587e8155a63668299fa1707044862dd6842b9f4c
Neta Hemyan <neta.he@gmail.com>
2018-04-19 18:27:06 +0300

    Made GameControls a controlled component

--- src/components/game-controls.js
+++ src/components/game-controls.js
@@ -3,16 +3,36 @@ import React, {Component} from 'react';
 export class GameControls extends Component {
     constructor(props) {
         super(props);
+        this.state = {width : 8, height : 8, mines :10};
 
         this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.handleChangeWidth = this.handleChangeWidth.bind(this);
+
+        this.handleChangeHeight = this.handleChangeHeight.bind(this);
+
+        this.handleChangeMines = this.handleChangeMines.bind(this);
+
+    }
+
+    handleChangeWidth(e) {
+            this.setState({width : e.target.value});
+    }
+
+    handleChangeHeight(e) {
+        this.setState({height : e.target.value});
+    }
+
+    handleChangeMines(e) {
+        this.setState({mines : e.target.value});
     }
 
     handleSubmit(e) {
         e.preventDefault();
-        const data = new FormData(e.target);
-        const width = data.get("width");
-        const height = data.get("height");
-        const mines = data.get("mines");
+
+        const width = this.state.width;
+        const height = this.state.height;
+        const mines = this.state.mines;
 
         this.props.startGame(width,height,mines);
     }
@@ -22,9 +42,9 @@ export class GameControls extends Component {
             <header>
                 <h1>Hello Minesweeper</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="width">Width: </label><input id="width" name="width" type="number"/>
-                    <label htmlFor="height">Height: </label><input id="height" name="height" type="number"/>
-                    <label htmlFor="mines">Mines: </label><input id="mines" name="mines" type="number"/>
+                    <label htmlFor="width">Width: </label><input id="width" name="width" type="number" value={this.state.width} onChange={this.handleChangeWidth}/>
+                    <label htmlFor="height">Height: </label><input id="height" name="height" type="number" value={this.state.height} onChange={this.handleChangeHeight}/>
+                    <label htmlFor="mines">Mines: </label><input id="mines" name="mines" type="number" value={this.state.mines} onChange={this.handleChangeMines}/>
                     <button>Start Game</button>
 
 
